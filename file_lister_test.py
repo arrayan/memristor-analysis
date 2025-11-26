80611ff2555572cf9b89c26347a0fe7506270982
illiazo <81965568+illiazo@users.noreply.github.com>
2025-11-26 17:01:29 +0100

    Basic GUI implementation and folder arragement (#21)
    * added poc for the pyside6 framework
    
    * added import functionality
    
    * Testing
    
    * help tab added
    
    * added help tab and made the window maximazeable
    
    * PullRequest Testing
    
    * Added a graph section
    
    * Removed help button and kept it in the menu
    
    * added requirements.txt and a short description how to set it up
    
    * added packages to requirements.txt
    
    * Added packages to requirements
    
    * Update graph_section.py
    
    +toolbar
    
    * Update graph_section.py
    
    * Update graph_section.py
    
    +reset, nach dem clear bleiben Titel und Graphstruktur
    
    * Update graph_section.py
    
    * Update graph_section.py
    
    Layout bleibt bei Größenänderung vom Fenster gleich
    
    * Added two poc-files
    file_lister for finding files in folder.
    pipeline for coverting an excel file to parquet, for faster handling
    
    * added file for basic math formulae
    
    * Arranged files in /src
    
    * latest commit
    
    * MEM-20 : removed f string without placeholder.
    
    * style: format Python code with Black
    
    * MEM-24 : Update GitHub Actions workflow to support more Python versions (#22)
    
    Co-authored-by: arrayan <34201300+arrayan@users.noreply.github.com>
    
    * MEM-21-Ruff
    
    ---------
    
    Co-authored-by: Julius von Stockhausen <134332070+P1ne4ppleP1zz4@users.noreply.github.com>
    Co-authored-by: arrayan <34201300+arrayan@users.noreply.github.com>
    Co-authored-by: Goya <159795519+Ryeong7@users.noreply.github.com>

---
+++ file_lister_test.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import sys
+
+
+def find_files() -> list:
+    """POC function, for finding all files in a root folder and storing them in a list"""
+    src = sys.argv[1]
+    print("Root folder:", src)
+
+    files = []
+    for path in Path(src).rglob("*.xls"):
+        files.append(path)
+
+    print("Filed found:", len(files))
+
+    return files
+
+
+print(find_files())
