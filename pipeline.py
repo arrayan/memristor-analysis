9c54ed88ee0ee73ca8e4f238c955f771a3a1f53e
arrayan <34201300+arrayan@users.noreply.github.com>
2025-11-30 23:17:51 +0100

    MEM-40-Updated Workflow for compataibilty with 3.11

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 
+
 def excel_to_parquet():
     path = Path(r"C:\Users\pineapple\Downloads\test.xls")
 
@@ -11,7 +12,7 @@ def excel_to_parquet():
         df = xl.parse(sheet_name)
 
         # Convert all object columns to string to avoid PyArrow errors
-        for col in df.select_dtypes(['object']):
+        for col in df.select_dtypes(["object"]):
             df[col] = df[col].astype(str)
 
         # Save to Parquet
@@ -19,20 +20,17 @@ def excel_to_parquet():
         df.to_parquet(out_path, engine="pyarrow")
         print("Saved:", out_path)
 
-    df_sheet = pd.read_parquet(Path(r"C:\Users\pineapple\Downloads\test.Cycle19.parquet"))
+    df_sheet = pd.read_parquet(
+        Path(r"C:\Users\pineapple\Downloads\test.Cycle19.parquet")
+    )
 
     plt.figure(figsize=(10, 6))
-    plt.plot(df_sheet['Time'], df_sheet['RESISTANCE'], label="Cycle19")
-    plt.xlabel('Time')
-    plt.ylabel('Resistance')
-    plt.title('Resistance over Time for Cycle19')
+    plt.plot(df_sheet["Time"], df_sheet["RESISTANCE"], label="Cycle19")
+    plt.xlabel("Time")
+    plt.ylabel("Resistance")
+    plt.title("Resistance over Time for Cycle19")
     plt.legend()
     plt.show()
 
 
 excel_to_parquet()
-
-
-
-
-
