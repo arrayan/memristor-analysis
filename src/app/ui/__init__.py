6cd0d36db77dbd3bae09f5f8cc9cef4e1e5c83fd
Julius von Stockhausen <134332070+P1ne4ppleP1zz4@users.noreply.github.com>
2026-01-31 00:27:28 +0100

    some refactoring

--- src/app/ui/__init__.py
+++ src/app/ui/__init__.py
@@ -1,9 +1,7 @@
 from.main_window import MainWindow
 from .menu_bar import MenuBar
 from .navigation_bar import NavigationBar
-from .side_bar import SideBar
 
 __all__ = ["MainWindow",
            "MenuBar",
-           "NavigationBar",
-           "SideBar"]
+           "NavigationBar"]
