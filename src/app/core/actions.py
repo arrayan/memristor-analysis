2df8a223854609940cec087e3ec048b51dc1b81d
Julius von Stockhausen <134332070+P1ne4ppleP1zz4@users.noreply.github.com>
2026-02-01 22:45:56 +0100

    Added Options to Action Enum + Refactoring for PR

--- src/app/core/actions.py
+++ src/app/core/actions.py
@@ -2,27 +2,32 @@ from enum import Enum, auto
 class MenuAction(Enum):
     # FILE MENU
     # IMPORT
-    IMPORT_DEVICE = ("Device", "Ctrl+O")
-    IMPORT_STACK = ("Stack", "Ctrl+Shift+O")
+    IMPORT_DEVICE = ("Device", "Ctrl+O", False)
+    IMPORT_STACK = ("Stack", "Ctrl+Shift+O", False)
 
     # EXPORT
-    EXPORT_ALL = ("All", "Ctrl+Shift+E")
-    EXPORT_ALL_PNG = ("PNG", None)
-    EXPORT_ALL_JPEG = ("JPEG", None)
-    EXPORT_ALL_APS = ("APS", None)
+    EXPORT_ALL = ("All", "Ctrl+Shift+E", False)
+    EXPORT_ALL_PNG = ("PNG", None, False)
+    EXPORT_ALL_JPEG = ("JPEG", None, False)
+    EXPORT_ALL_APS = ("APS", None, False)
     
-    EXPORT_CURRENT = ("Current", "Ctrl+E")
-    EXPORT_CURRENT_PNG = ("PNG", None)
-    EXPORT_CURRENT_JPEG = ("JPEG", None)
-    EXPORT_CURRENT_APS = ("APS", None)
+    EXPORT_CURRENT = ("Current", "Ctrl+E", False)
+    EXPORT_CURRENT_PNG = ("PNG", None, False)
+    EXPORT_CURRENT_JPEG = ("JPEG", None, False)
+    EXPORT_CURRENT_APS = ("APS", None, False)
 
     # EXIT
-    EXIT = ("Exit", "Ctrl+Q")
+    EXIT = ("Exit", "Ctrl+Q", False)
+
+    # OPTIONS MENU
+    SCALE_LINEAR = ("linear", None, True)
+    SCALE_LOG = ("log", None, True)
     
     # HELP MENU
-    VIEW_HELP = ("View Help", "F1")
-    ABOUT = ("About", None)
+    VIEW_HELP = ("View Help", "F1", False)
+    ABOUT = ("About", None, False)
 
-    def __init__(self, text, shortcut=None):
+    def __init__(self, text, shortcut=None, checkable=False):
         self.text = text
         self.shortcut = shortcut
+        self.checkable = checkable
--- src/app/ui/menu_bar.py
+++ src/app/ui/menu_bar.py
@@ -1,6 +1,6 @@
 import PySide6.QtWidgets as qt
 from PySide6.QtGui import QAction, QActionGroup
-from core import MenuActions
+from core import MenuAction
 
 class MenuBar(qt.QMenuBar):
     def __init__(self, parent=None):
@@ -10,11 +10,12 @@ class MenuBar(qt.QMenuBar):
 
     def setup_ui(self):
         # Create Actions from enum
-        for action in MenuActions:
+        for action in MenuAction:
             menu_action = QAction(action.text, self)
             if action.shortcut:
                 menu_action.setShortcut(action.shortcut)
-            
+            if action.checkable:
+                menu_action.setCheckable(True)
             # Store it so we can access it later
             self.menu_actions[action] = menu_action
 
@@ -27,31 +28,31 @@ class MenuBar(qt.QMenuBar):
         self.file_menu = self.addMenu("File")
 
         self.file_menu.addSection("Import")
-        self.file_menu.addAction(self.menu_actions[MenuActions.IMPORT_DEVICE])
-        self.file_menu.addAction(self.menu_actions[MenuActions.IMPORT_STACK])
+        self.file_menu.addAction(self.menu_actions[MenuAction.IMPORT_DEVICE])
+        self.file_menu.addAction(self.menu_actions[MenuAction.IMPORT_STACK])
         self.file_menu.addSeparator()
 
         self.file_menu.addSection("Export")
         self.file_sub_menu_export_all = self.file_menu.addMenu("All To")
         self.file_sub_menu_export_current = self.file_menu.addMenu("Current To")
-        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuActions.EXPORT_ALL_PNG])
-        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuActions.EXPORT_ALL_JPEG])
-        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuActions.EXPORT_ALL_APS])
-        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuActions.EXPORT_CURRENT_PNG])
-        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuActions.EXPORT_CURRENT_JPEG])
-        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuActions.EXPORT_CURRENT_APS])
-        self.file_menu.addAction(self.menu_actions[MenuActions.EXPORT_ALL])
-        self.file_menu.addAction(self.menu_actions[MenuActions.EXPORT_CURRENT])
+        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuAction.EXPORT_ALL_PNG])
+        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuAction.EXPORT_ALL_JPEG])
+        self.file_sub_menu_export_all.addAction(self.menu_actions[MenuAction.EXPORT_ALL_APS])
+        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuAction.EXPORT_CURRENT_PNG])
+        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuAction.EXPORT_CURRENT_JPEG])
+        self.file_sub_menu_export_current.addAction(self.menu_actions[MenuAction.EXPORT_CURRENT_APS])
+        self.file_menu.addAction(self.menu_actions[MenuAction.EXPORT_ALL])
+        self.file_menu.addAction(self.menu_actions[MenuAction.EXPORT_CURRENT])
         self.file_menu.addSeparator()
 
-        self.file_menu.addAction(self.menu_actions[MenuActions.EXIT])
+        self.file_menu.addAction(self.menu_actions[MenuAction.EXIT])
 
     def setup_options_menu(self):
         self.options_menu = self.addMenu("Options")
         scale = QActionGroup(self)
         scale.setExclusive(True)
-        scale_lin = QAction("linear", self, checkable=True)
-        scale_log = QAction("linear", self, checkable=True)
+        scale_lin = self.menu_actions[MenuAction.SCALE_LINEAR]
+        scale_log = self.menu_actions[MenuAction.SCALE_LOG]
         scale.addAction(scale_lin)
         scale_lin.setChecked(True)
         scale.addAction(scale_log)
@@ -59,10 +60,6 @@ class MenuBar(qt.QMenuBar):
         self.options_menu.addSection("Scale")
         self.options_menu.addActions([scale_lin, scale_log])
 
-
-        return
-
     def setup_help_menu(self):
         self.help_menu = self.addMenu("Help")
-        self.action_view_help = QAction("View Help", self)
-        self.help_menu.addAction(self.action_view_help)
+        self.help_menu.addAction(self.menu_actions[MenuAction.VIEW_HELP])
