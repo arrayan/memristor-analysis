0aba300c147647d10bbffb493a9a2979fb34a0bf
arrayan <34201300+arrayan@users.noreply.github.com>
2026-02-20 16:17:12 +0100

    MEM-103 : Fix import paths and adjust sys.path for module resolution

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,11 @@
 import sys
+import os
 import PySide6.QtWidgets as qt
-from ui import MainWindow
+from src.app.ui import MainWindow
 
+
+if __package__ is None:
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 def main():
     app = qt.QApplication(sys.argv)
