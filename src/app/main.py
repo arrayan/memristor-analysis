2660f952775ce632ef292acebdeee66d976674cc
Julius von Stockhausen <134332070+P1ne4ppleP1zz4@users.noreply.github.com>
2026-02-21 09:08:00 +0100

    Removed duplicate path insert.

--- src/app/main.py
+++ src/app/main.py
@@ -3,13 +3,6 @@ import os
 import PySide6.QtWidgets as qt
 from src.app.ui import MainWindow
 
-
-if __package__ is None:
-    sys.path.insert(
-        0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    )
-
-
 if __package__ is None:
     sys.path.insert(
         0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
