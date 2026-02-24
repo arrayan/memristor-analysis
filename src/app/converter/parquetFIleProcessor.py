e2fae2ea1f3198190c5642f04aea01abded55b52
goya <ghl990125@gmail.com>
2026-02-24 01:38:05 +0100

    add missing file

---
+++ src/app/converter/parquetFIleProcessor.py
@@ -0,0 +1,27 @@
+import polars as pl
+from pathlib import Path
+from .models import ProcessingResult
+import time
+
+class ParquetFileProcessor:
+
+    def process(self, file_path):
+
+        file_id = Path(file_path).stem
+
+        result = ProcessingResult(file_id = file_id)
+
+        start = time.time()
+
+        try:
+            data_frame = pl.read_parquet(file_path)
+            result.cycles_df = data_frame
+
+        except Exception as e:
+            result.warnings.append(str(e))
+
+        end = time.time()
+
+        result.elapsed = end - start
+
+        return result
